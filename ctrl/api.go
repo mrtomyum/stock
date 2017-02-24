e39f57359b8c658f6fec4d4e363ea9cf06c51cc7
Kasem Arnontavilas <mrtomyum@gmail.com>
2017-02-24 09:23:14 +0000

    ลองปิดฟังค์ชั่นที่ไม่จำเป็นดู

--- ctrl/api.go
+++ ctrl/api.go
@@ -34,46 +34,46 @@ type Link struct {
 }
 
 // Structure for collection of search string for frontend request.
-type Search struct {
-	Name string
-}
+//type Search struct {
+//	Name string
+//}
 
-func (rs ResponseStatus) MarshalJSON() ([]byte, error) {
-	statusString, ok := map[ResponseStatus]string{
-		SUCCESS: "success",
-		FAIL:    "fail",
-		ERROR:   "error",
-	}[rs]
-	if !ok {
-		return nil, fmt.Errorf("invalid ResponseStatus value %v", rs)
-	}
-	return json.Marshal(statusString)
-}
+//func (rs ResponseStatus) MarshalJSON() ([]byte, error) {
+//	statusString, ok := map[ResponseStatus]string{
+//		SUCCESS: "success",
+//		FAIL:    "fail",
+//		ERROR:   "error",
+//	}[rs]
+//	if !ok {
+//		return nil, fmt.Errorf("invalid ResponseStatus value %v", rs)
+//	}
+//	return json.Marshal(statusString)
+//}
 
-func (rs *ResponseStatus) UnmarshalJSON(data []byte) error {
-	// TODO: This method is not TEST yet!
-	// to receive response status from other service in JSON
-	// convert it to ENUM
-	var s string
-	if err := json.Unmarshal(data, &s); err != nil {
-		return fmt.Errorf("ResponseStatus should be a string, got %s", data)
-	}
-	s = strings.ToLower(s)
-	statusENUM, ok := map[string]ResponseStatus{
-		"success": SUCCESS,
-		"fail":    FAIL,
-		"error":   ERROR,
-	}[s]
-	if !ok {
-		return fmt.Errorf("invalid Response Status %q", s)
-	}
-	*rs = statusENUM
-	return nil
-}
+//func (rs *ResponseStatus) UnmarshalJSON(data []byte) error {
+//	// TODO: This method is not TEST yet!
+//	// to receive response status from other service in JSON
+//	// convert it to ENUM
+//	var s string
+//	if err := json.Unmarshal(data, &s); err != nil {
+//		return fmt.Errorf("ResponseStatus should be a string, got %s", data)
+//	}
+//	s = strings.ToLower(s)
+//	statusENUM, ok := map[string]ResponseStatus{
+//		"success": SUCCESS,
+//		"fail":    FAIL,
+//		"error":   ERROR,
+//	}[s]
+//	if !ok {
+//		return fmt.Errorf("invalid Response Status %q", s)
+//	}
+//	*rs = statusENUM
+//	return nil
+//}
 
-func (rs *Response) Success(data interface{}) {
-	var c gin.Context
-	rs.Status = SUCCESS
-	rs.Data = data
-	c.JSON(http.StatusOK, rs)
-}
+//func (rs *Response) Success(data interface{}) {
+//	var c gin.Context
+//	rs.Status = SUCCESS
+//	rs.Data = data
+//	c.JSON(http.StatusOK, rs)
+//}
